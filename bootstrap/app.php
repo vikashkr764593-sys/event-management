6bc84052c41912a406a6c06e21f932ea2bdd9e4b
Vikash Kumar <vikash.kumar@transneuron.com>
2026-04-29 23:48:20 +0530

    Trust proxies to fix CSS HTTPS mixed content

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/admin/login');
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
