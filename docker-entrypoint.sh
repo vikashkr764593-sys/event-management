5dae18209263be3574294f3f126f5146a848f383
Vikash Kumar <vikash.kumar@transneuron.com>
2026-04-29 23:40:21 +0530

    Fix cache permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,5 +12,9 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+# Fix permissions since artisan commands might have created files as root
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Start Apache in foreground
 exec apache2-foreground
