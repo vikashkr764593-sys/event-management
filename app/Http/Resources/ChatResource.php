733bad6ceb3002ebbf769b43597c56f7d9cd1e45
Vikash Kumar <vikash.kumar@transneuron.com>
2026-05-06 12:22:30 +0530

    Some changes

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -1,8 +1,13 @@
 <?php
+
 namespace App\Http\Resources;
+
 use Illuminate\Http\Resources\Json\JsonResource;
-class ChatResource extends JsonResource {
-    public function toArray($request) {
+
+class ChatResource extends JsonResource
+{
+    public function toArray($request)
+    {
         return ['id' => $this->id, 'booking_id' => $this->booking_id, 'sender' => new UserResource($this->whenLoaded('sender')), 'message' => $this->message, 'created_at' => $this->created_at];
     }
 }
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -1,4 +1,13 @@
 <?php
+
 namespace App\Http\Resources;
+
 use Illuminate\Http\Resources\Json\JsonResource;
-class UserResource extends JsonResource { public function toArray($request) { return ['id' => $this->id, 'name' => $this->name, 'email' => $this->email, 'role' => $this->role]; } }
+
+class UserResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return ['id' => $this->id, 'name' => $this->name, 'email' => $this->email, 'role' => $this->role];
+    }
+}
